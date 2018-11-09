96903af3499b1cce183643bdefc37b8b24c83b8c
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-09 11:47:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+import './App.css';
+import Headder from './components/headder';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Headder />
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
