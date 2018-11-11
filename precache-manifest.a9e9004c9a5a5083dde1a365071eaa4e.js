67ec8a5649ec02686db3f40a1ac46b2a017c4ae4
Tatsuya Asami <sharekobekko@gmail.com>
2018-11-11 23:28:42 +0900

    Updates

--- precache-manifest.a9e9004c9a5a5083dde1a365071eaa4e.js
+++ precache-manifest.a9e9004c9a5a5083dde1a365071eaa4e.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/portfolio-astatsuya/static/js/runtime~main.0435a205.js"
   },
   {
-    "revision": "7bea35a859e5068d8e6c",
-    "url": "/portfolio-astatsuya/static/js/main.7bea35a8.chunk.js"
+    "revision": "9e6cb8039aba28e721b2",
+    "url": "/portfolio-astatsuya/static/js/main.9e6cb803.chunk.js"
   },
   {
     "revision": "dd5452b09a4b75d9e63e",
     "url": "/portfolio-astatsuya/static/js/1.dd5452b0.chunk.js"
   },
   {
-    "revision": "e4bedd705439bc3260ca22a7f20d795c",
+    "revision": "a05f32f571a67c3d2f886a38dfb8ceff",
     "url": "/portfolio-astatsuya/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-astatsuya/precache-manifest.9ba4bafd2cac32796e6c9657eb7f4144.js"
+  "/portfolio-astatsuya/precache-manifest.a9e9004c9a5a5083dde1a365071eaa4e.js"
 );
 
 workbox.clientsClaim();
