cd7fa427b86e5a5ebef3ae254129f90249d0ccd2
Tatsuya Asami <sharekobekko@gmail.com>
2018-12-07 22:50:15 +0900

    add github link in contents

--- src/components/works.js
+++ src/components/works.js
@@ -16,6 +16,10 @@ const Works = () => {
             <br />
             <p>性別、年齢、役職など指定した条件から、異動までの平均年数と異動先を検索する。</p>
             <p>React, Redux, React-Routerを使用。</p>
+            <p>Github:
+              <a href="https://github.com/astatsuya/transfer" target="_blank" rel="noopener noreferrer">https://github.com/astatsuya/transfer
+              </a>
+            </p>
           </Col>
           <Col xs={12} md={6} style={{maxWidth: 900}}>
             <Image src="images/transfer.png" alt="" responsive thumbnail />
@@ -31,6 +35,10 @@ const Works = () => {
             <br />
             <p>360度写真をAndroid, iOSではジャイロセンサー、その他デバイスではドラッグで視点操作できる。</p>
             <p>JavaScript, three.jsを使用。</p>
+            <p>Github:
+              <a href="https://github.com/astatsuya/three360" target="_blank" rel="noopener noreferrer">https://github.com/astatsuya/three360
+              </a>
+            </p>
           </Col>
           <Col xs={12} md={6} style={{maxWidth: 900}}>
             <Image src="images/360.png" alt="" responsive thumbnail />
@@ -46,6 +54,10 @@ const Works = () => {
             <br />
             <p>スポーツ等のパフォーマンスを4段階で記録できる。</p>
             <p>Reactを使用。</p>
+            <p>Github:
+              <a href="https://github.com/astatsuya/scout" target="_blank" rel="noopener noreferrer">https://github.com/astatsuya/scout
+              </a>
+            </p>
           </Col>
           <Col xs={12} md={6} style={{maxWidth: 900}}>
             <Image src="images/Scout.png" alt="" responsive thumbnail />
@@ -61,6 +73,10 @@ const Works = () => {
             <br />
             <p>こちらのサイト</p>
             <p>React, React-BootStrapを使用。</p>
+            <p>Github:
+              <a href="https://github.com/astatsuya/portfolio-astatsuya" target="_blank" rel="noopener noreferrer">https://github.com/astatsuya/portfolio-astatsuya
+              </a>
+            </p>
           </Col>
           <Col xs={12} md={6} style={{maxWidth: 900}}>
             <Image src="images/Portfolio.png" alt="" responsive thumbnail />
@@ -76,6 +92,10 @@ const Works = () => {
             <br />
             <p>デュースは2点差になるまで継続、3セット先取、5セット目は15点マッチといった公式ルールを適用。</p>
             <p>JavaScript, HTML5, CSS3を使用。</p>
+            <p>Github:
+              <a href="https://github.com/astatsuya/scoreboard" target="_blank" rel="noopener noreferrer">https://github.com/astatsuya/scoreboard
+              </a>
+            </p>
           </Col>
           <Col xs={12} md={6} style={{maxWidth: 900}}>
             <Image src="images/Scoreboard.png" alt="" responsive thumbnail />
